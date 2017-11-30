6d69e2f9d9dd2c4ceb596c0877842def5a05c199
KENDY\kjmas <kjmaster3@hotmail.com>
2017-11-30 04:27:11 -0500

    update to kendyBranch

---
+++ Assets/Scripts/LavaBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaBehavior : MonoBehaviour {
+
+    Vector3 startPosition;
+    Vector3 endPosition;
+    public float speed;
+
+    private void Start()
+    {
+        Vector3 startPosition = transform.position;
+        Vector3 endPosition = new Vector3(99.36f, 9.56f, 0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(transform.position == endPosition))
+        {
+           transform.position = Vector3.Lerp(new Vector3(99.36f, -4.21f, 0f), endPosition, speed * Time.deltaTime);
+        }
+    }
+}
