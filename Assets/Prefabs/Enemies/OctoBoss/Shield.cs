f71429d102feaf6a0695b2869c3cdc0440bedc2f
Schayik <pimvschayik@live.nl>
2017-11-29 22:27:56 -0500

    Hi

---
+++ Assets/Prefabs/Enemies/OctoBoss/Shield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "DamagingBullet")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
