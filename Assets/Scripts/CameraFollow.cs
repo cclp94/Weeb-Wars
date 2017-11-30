ddb6652c1c885dfd8c5bdad9c4029656120d654f
Nicholas Chiasson-Palacios <ni_chias@encs.concordia.ca>
2017-11-30 16:27:26 -0500

    Camera toggle

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -24,6 +24,8 @@ public class CameraFollow : MonoBehaviour
 
     Vector3 originalPos;
 
+    bool toggle;
+
     void Update()
     {
         if (mTarget != null)
@@ -42,7 +44,9 @@ public class CameraFollow : MonoBehaviour
                 transform.Translate(direction * kFollowSpeed * Time.deltaTime);
 =======*/
             // Camera that follows mouse
-            if (Input.GetKey(KeyCode.LeftControl))
+            if (Input.GetKeyDown(KeyCode.LeftControl)) toggle = !toggle;
+
+            if (toggle)
             {
                 Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - mTarget.transform.position;
                 Vector3 targetPosition;
