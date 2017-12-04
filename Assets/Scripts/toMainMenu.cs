68f299bf5b7c29babfa0ab5eae483bf16051bac7
Schayik <pimvschayik@live.nl>
2017-12-04 18:18:05 -0500

    Finished Tutorial Level

---
+++ Assets/Scripts/toMainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toMainMenu : MonoBehaviour {
+
+    [SerializeField]
+    GameObject pause;
+    PauseMenu script;
+
+    void Start()
+    {
+        script = pause.GetComponent<PauseMenu>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            script.CompleteLevel();
+        }
+    }
+}
