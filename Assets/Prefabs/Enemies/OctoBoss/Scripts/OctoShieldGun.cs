120275a8e5ec97d5c2a913363c6305bf36242707
Schayik <pimvschayik@live.nl>
2017-12-04 10:29:49 -0500

    Fixed OctoBoss

---
+++ Assets/Prefabs/Enemies/OctoBoss/Scripts/OctoShieldGun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctoShieldGun : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //col.gameObject.GetComponent<PlayerUpgradeManager>().unlockWeapon("Shield");
+           Destroy(gameObject);
+        }
+    }
+
+}
